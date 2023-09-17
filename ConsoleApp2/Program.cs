e4c9aa1bf913a32f6941d7c4a956c0f9993ee750
totoqqwe <anton.stepanchenko14@gmail.com>
2023-09-17 17:01:09 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static ConsoleApp2.Address;
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Address address = new Address("12345", "Україна", "Київ", "Головна", "42", "10");
+
+            Console.WriteLine("Індекс: " + address.Index);
+            Console.WriteLine("Країна: " + address.Country);
+            Console.WriteLine("Місто: " + address.City);
+            Console.WriteLine("Вулиця: " + address.Street);
+            Console.WriteLine("Будинок: " + address.House);
+            Console.WriteLine("Квартира: " + address.Apartment);
+
+            Console.ReadKey();
+        }
+    }
+}
